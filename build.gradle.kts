f9a768780c32ec7199c10b5efd7c4af185e92918
marco rolappe <215132+mrolappe@users.noreply.github.com>
2023-08-25 11:43:33 +0200

    upgrade gradle version, change to kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+//    id 'org.jetbrains.kotlin.jvm' version '1.3.20'
+    kotlin("jvm") version "1.9.10"
+//    id 'application'
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+//    implementation "org.jetbrains.kotlin:kotlin-stdlib"
+
+//    testCompile 'org.jetbrains.kotlin:kotlin-test'
+//    testCompile 'junit:junit:4.12'
+}
+
+//application {
+//    mainClassName = 'jmj.deskumm.DeskummKt'
+//}
