2b248e1ab61a854b574e901955772ca4ae60deac
Marco Rolappe <215132+mrolappe@users.noreply.github.com>
2025-11-22 15:27:34 +0100

    Add kotlin-result dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
     implementation("org.slf4j:slf4j-api:2.0.16")
     implementation("ch.qos.logback:logback-classic:1.5.21")
+
+    implementation("com.michael-bull.kotlin-result:kotlin-result:2.1.0")
 //    implementation "org.jetbrains.kotlin:kotlin-stdlib"
 
 //    testCompile 'org.jetbrains.kotlin:kotlin-test'
